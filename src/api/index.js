1f80d8448e9b0c5a4e5d6ed85a16df39e7a7874e
Jurayev311 <joxauzking@gmail.com>
2025-08-23 18:27:45 +0500

    Add Access-Control-Allow-Origin header to API request configuration

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const request = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
-    "hl": localStorage.getItem("lang") || "en"
+    "hl": localStorage.getItem("lang") || "en",
+    "Access-Control-Allow-Origin": "http://localhost:3000"
   }
 })
 
